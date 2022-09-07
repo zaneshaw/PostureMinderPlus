b702f9be76a4acde522028dac9032306fbd4ef24
Zane Shaw <admin@triangle100.com>
2022-09-07 22:42:46 +1000

    Refactor service worker

--- js/background.js
+++ js/background.js
@@ -1,32 +1,48 @@
-// chrome.alarms.create("check", { delayInMinutes: 0.1, periodInMinutes: 0.1 });
+const app = {};
 
-chrome.alarms.onAlarm.addListener(function (alarm) {
-	if (alarm.name == "check") {
-		displayNotif();
-	}
-});
-
-chrome.runtime.onMessage.addListener((msg) => {
-	if (msg = "debug") displayNotif();
-});
-
-function displayNotif() {
-	const options = {
-		type: "basic",
-		title: "Posture Check!",
-		message: "Were you sitting up straight?",
-		iconUrl: "/img/spine.png",
-		buttons: [{ title: "Yes" }, { title: "No" }],
-		requireInteraction: false
-	};
-
-	chrome.notifications.getPermissionLevel(function (permission) {
-		if (permission === "granted") {
-			chrome.notifications.create("notification", options);
-
-			setTimeout(function () {
-				chrome.notifications.clear("notification");
-			}, 5000);
-		}
-	});
+app.init = () => {
+	// app.reminder.init();
+	app.listeners.init();
 }
+
+app.reminder = {
+	init: () => {
+		chrome.alarms.create("reminder", { delayInMinutes: 0.1, periodInMinutes: 0.1 });
+
+		chrome.alarms.onAlarm.addListener(function (alarm) {
+			if (alarm.name == "reminder") {
+				app.reminder.display();
+			}
+		});
+	},
+	display: () => {
+		const options = {
+			type: "basic",
+			title: "Posture Check!",
+			message: "Were you sitting up straight?",
+			iconUrl: "/img/spine.png",
+			buttons: [{ title: "Yes" }, { title: "No" }],
+			requireInteraction: false
+		};
+
+		chrome.notifications.getPermissionLevel(function (permission) {
+			if (permission === "granted") {
+				chrome.notifications.create("notification", options);
+
+				setTimeout(function () {
+					chrome.notifications.clear("notification");
+				}, 5000);
+			}
+		});
+	}
+}
+
+app.listeners = {
+	init: () => {
+		chrome.runtime.onMessage.addListener((msg) => {
+			if (msg = "debug") app.reminder.display();
+		});
+	}
+}
+
+app.init();
