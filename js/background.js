9451665a5167c74f131f56bd71107e51be9fe63e
Zane Shaw <zaneshaw.home@gmail.com>
2022-09-08 21:53:56 +1000

    Add basic storage functionality

--- js/background.js
+++ js/background.js
@@ -2,6 +2,7 @@ const app = {};
 
 app.init = () => {
 	// app.reminder.init();
+	app.storage.init();
 	app.listeners.init();
 }
 
@@ -37,10 +38,40 @@ app.reminder = {
 	}
 }
 
+app.storage = {
+	init: () => {
+
+	},
+	data: async () => {
+		let keys = await chrome.storage.sync.get(null);
+		return keys;
+	},
+	increment: async (key) => {
+		let data = await app.storage.data();
+		val = data[key] || 0;
+
+		let newVal = val + 1;
+
+		chrome.storage.sync.set({ [key]: newVal });
+
+		console.log(`Updated '${key}' (${val} => ${newVal})`);
+	}
+}
+
 app.listeners = {
 	init: () => {
 		chrome.runtime.onMessage.addListener((msg) => {
-			if (msg = "debug") app.reminder.display();
+			if (msg == "debug") app.reminder.display();
+		});
+
+		chrome.notifications.onButtonClicked.addListener(function (id, i) {
+			if (id === "notification") {
+				if (i === 0) {
+					app.storage.increment("c-yes");
+				} else if (i === 1) {
+					app.storage.increment("c-no");
+				}
+			}
 		});
 	}
 }
