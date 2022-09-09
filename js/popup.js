41aca4a954c5dd57c841c3a77780330432990f63
Zane Shaw <zaneshaw.home@gmail.com>
2022-09-09 14:51:50 +1000

    Add popup object

--- js/popup.js
+++ js/popup.js
@@ -1,36 +1,46 @@
-$("#debug-dialog").click(() => {
-	chrome.runtime.sendMessage({ debug: "dialog" });
-});
-$("#debug-clear").click(() => {
-	chrome.runtime.sendMessage({ debug: "clear" });
-});
-$("#c-yes-btn").click(() => {
-	chrome.runtime.sendMessage({ debug: { choice: "yes" } });
-});
-$("#c-no-btn").click(() => {
-	chrome.runtime.sendMessage({ debug: { choice: "no" } });
-});
-$("#c-ignore-btn").click(() => {
-	chrome.runtime.sendMessage({ debug: { choice: "ignore" } });
-});
+const popup = {};
 
-chrome.runtime.onMessage.addListener((request) => {
-	$("#msg").text(JSON.stringify(request)); // Debug line
-
-	if (request.payload) {
-		updateDebugText(request.payload);
-	}
-});
-
-chrome.runtime.sendMessage("getData", (response) => {
-	$("#msg").text(JSON.stringify(response)); // Debug line
-
-	updateDebugText(response);
-});
-
-function updateDebugText(data) {
-	$("#c-yes").text(data["c-yes"] || "-");
-	$("#c-no").text(data["c-no"] || "-");
-	$("#c-ignore").text(data["c-ignore"] || "-");
+popup.init = () => {
+	popup.debug.init();
 }
 
+popup.debug = {
+	init: () => {
+		$("#debug-dialog").click(() => {
+			chrome.runtime.sendMessage({ debug: "dialog" });
+		});
+		$("#debug-clear").click(() => {
+			chrome.runtime.sendMessage({ debug: "clear" });
+		});
+		$("#c-yes-btn").click(() => {
+			chrome.runtime.sendMessage({ debug: { choice: "yes" } });
+		});
+		$("#c-no-btn").click(() => {
+			chrome.runtime.sendMessage({ debug: { choice: "no" } });
+		});
+		$("#c-ignore-btn").click(() => {
+			chrome.runtime.sendMessage({ debug: { choice: "ignore" } });
+		});
+
+		chrome.runtime.onMessage.addListener((request) => {
+			$("#msg").text(JSON.stringify(request)); // Debug line
+
+			if (request.payload) {
+				popup.debug.updateDebugText(request.payload);
+			}
+		});
+
+		chrome.runtime.sendMessage("getData", (response) => {
+			$("#msg").text(JSON.stringify(response)); // Debug line
+
+			popup.debug.updateDebugText(response);
+		});
+	},
+	updateDebugText: (data) => {
+		$("#c-yes").text(data["c-yes"] || "-");
+		$("#c-no").text(data["c-no"] || "-");
+		$("#c-ignore").text(data["c-ignore"] || "-");
+	}
+}
+
+popup.init();
